cb6cb2f4cccf0d9bb26d9ccb6c16f191448d794c
hunterlan <konstantin.sharykin@outlook.com>
2024-06-10 00:06:24 +0200

    Implemented classification service

---
+++ src/app/models/classification-result.ts
@@ -0,0 +1,4 @@
+export interface ClassificationResult {
+  reconstructed: string;
+  loss: number;
+}
--- src/app/services/classification.service.ts
+++ src/app/services/classification.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ClassificationResponse} from "../models/classification-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassificationService {
+  private readonly path: string = '/classify';
+  constructor(private readonly httpClient: HttpClient) { }
 
-  constructor() { }
+  require(image: string) {
+    return this.httpClient.post<ClassificationResponse>(this.path, {"image": image});
+  }
 }
