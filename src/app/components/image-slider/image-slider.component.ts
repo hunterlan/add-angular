83ed5a5d400fbfa8ed1db37d3a87082e4b8c59bb
hunterlan <konstantin.sharykin@outlook.com>
2024-06-11 09:13:29 +0200

    Slider is done

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -11,4 +11,20 @@ export class ImageSliderComponent {
   @Input() images: string[] = [];
 
   currentIndexImage: number = 0;
+
+  goToPrevious(): void {
+    const isFirstSlide = this.currentIndexImage === 0;
+    const newIndex = isFirstSlide
+      ? this.images.length - 1
+      : this.currentIndexImage - 1;
+
+    this.currentIndexImage = newIndex;
+  }
+
+  goToNext(): void {
+    const isLastSlide = this.currentIndexImage === this.images.length - 1;
+    const newIndex = isLastSlide ? 0 : this.currentIndexImage + 1;
+
+    this.currentIndexImage = newIndex;
+  }
 }
